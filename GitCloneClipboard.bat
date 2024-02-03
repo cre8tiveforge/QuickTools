a7d81d4850d8e1ea80ae7e6218787cef0c412903
cre8tiveforge <158652101+cre8tiveforge@users.noreply.github.com>
2024-02-02 19:43:02 -0600

    Update GitCloneClipboard.bat

--- GitCloneClipboard.bat
+++ GitCloneClipboard.bat
@@ -2,4 +2,5 @@
 echo Getting URL from clipboard...
 powershell -command "Add-Type -AssemblyName System.Windows.Forms; $clipboard = [System.Windows.Forms.Clipboard]::GetText(); git clone $clipboard"
 echo Done.
-pause
+:: pause
+:: remove part before pause is you want the window to stay open
